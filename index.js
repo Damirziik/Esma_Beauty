832cec866c8b17487c4cf58519a2ee2fe3a65d53
Damirziik <khamidovdamir02@gmail.com>
2025-07-30 11:43:31 +0500

    Update File

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-    // Открытие модалки
-    document.querySelectorAll('.more-btn').forEach(btn => {
+// Открытие модалки
+document.querySelectorAll('.more-btn').forEach(btn => {
     btn.addEventListener('click', () => {
         const targetId = btn.getAttribute('data-modal');
         const modal = document.getElementById(targetId);
@@ -8,42 +7,42 @@
     });
 });
 
-    // Закрытие модалки
-    document.querySelectorAll('[data-close]').forEach(btn => {
+// Закрытие модалки
+document.querySelectorAll('[data-close]').forEach(btn => {
     btn.addEventListener('click', () => {
         btn.closest('.modal').style.display = 'none';
     });
 });
 
-    // Клик вне модалки
-    window.addEventListener('click', e => {
+// Клик вне модалки
+window.addEventListener('click', e => {
     if (e.target.classList.contains('modal')) {
-    e.target.style.display = 'none';
-}
+        e.target.style.display = 'none';
+    }
 });
 
-        const links = document.querySelectorAll('.more-link');
-        const modal = document.getElementById('info-modal');
-        const modalText = document.getElementById('modal-text');
-        const closeBtn = modal.querySelector('.close');
+const links = document.querySelectorAll('.more-link');
+const modal = document.getElementById('info-modal');
+const modalText = document.getElementById('modal-text');
+const closeBtn = modal.querySelector('.close');
 
-        links.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            const info = link.getAttribute('data-info');
-            modalText.textContent = info;
-            modal.style.display = 'block';
-        });
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const info = link.getAttribute('data-info');
+        modalText.textContent = info;
+        modal.style.display = 'block';
     });
+});
 
-        closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
-    });
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
 
-        window.addEventListener('click', (e) => {
-        if (e.target === modal) {
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
         modal.style.display = 'none';
     }
-    });
+});
 
 
